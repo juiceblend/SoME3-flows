080dec03c84e635777e1421a72effd750159771d
Justin Chen <justinchenyay@gmail.com>
2023-07-20 15:14:02 -0400

    added comments

--- card_network.py
+++ card_network.py
@@ -6,6 +6,7 @@ class Cards(Scene):
 
     def construct(self):
 
+        # First initialize nodes
         start = Node([-5,0,0], 'A')
         end = Node([5,0,0], 'B')
 
@@ -16,14 +17,17 @@ class Cards(Scene):
             piles[i] = Node([-2,-3 + i/2,0], str(i))
             suits[i] = Node([2,-3 + i/2,0], str(i))
 
+        # Initialize graph
         graph = Network([start] + piles + suits + [end])
 
+        # Add edges
         for i in range(13):
             graph.add_edge(start, piles[i])
             graph.add_edge(suits[i], end)
             for j in range(13):
                 graph.add_edge(piles[i], suits[j])
         
+        # Convert to manim
         Nodes, Edges = graph.to_manim()
     
         self.play(Write(VGroup(Nodes, Edges)), run_time = 3)
