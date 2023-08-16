f9bf893e488bb30a561a7a2871b92eb1099f2bd5
Andres Arcia <aaa201@cam.ac.uk>
2023-08-16 17:42:22 +0100

    Cleaned up script using Deck object

--- dealing_cards.py
+++ dealing_cards.py
@@ -1,30 +1,17 @@
 from manim import *
-import random
+from Deck import *
 
-class Card(Scene):
-
-    def get_text(card):
-        card_text = card.submobjects
-        return card_text[0].text
+class Deal(Scene):
     
     def construct(self):
-        colors = [PURPLE, RED, DARK_BLUE, GREEN_E]
-        # A deck of 52 squares (cards) with their indices (rank) written on them
-        deck = []
-        for i in range(52):
-            square = Square(fill_opacity=0.9, fill_color=LIGHT_GREY, color=colors[i // 13])
-            text = Text(str(i % 13), color=colors[i // 13])
-            text.align_to(square.get_corner(UP+LEFT), UP+LEFT).shift(RIGHT*0.3 + DOWN*0.3)
-            deck.append(square.add(text).scale(0.5))
 
+        deck_obj = Deck()
+        deck = deck_obj.cards
         # 13 empty piles
         piles = [[] for _ in range(13)]
 
         # Two rows for the piles, with more spacing
         locations = [(i*2 - 6 + j)*RIGHT + (j-3+2*j)*DOWN for j in range(2) for i in range(7 if j == 0 else 6)]
-
-        # Shuffle the deck
-        random.shuffle(deck)
 
         for i in range(52):
             # Add each card to a pile
